40221eae2c64d0d6ae7724f16900721a104dc9e0
Youdon <axyzdong@gmail.com>
2026-05-24 19:40:01 +0800

    Refactor to use arxiv.Client for results retrieval

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -110,8 +110,10 @@ def get_daily_papers(topic, query=" ", max_results=2):
         max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate
     )
-
-    for result in search_engine.results():
+  
+    client = arxiv.Client()
+  
+    for result in client.results(search_engine):
 
         paper_id = result.get_short_id()
         paper_title = result.title
